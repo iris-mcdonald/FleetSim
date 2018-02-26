7d895f2c53bf9ea84c8635d3d4dfb37024c1997b
iris-mcdonald <32845453+iris-mcdonald@users.noreply.github.com>
2018-02-25 20:53:17 -0500

    Add files via upload

--- FleetSim.Objects/Properties/AssemblyInfo.cs
+++ FleetSim.Objects/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f244ddf9-2d94-4f0e-b38d-995941b5917b")]
+[assembly: Guid("d98f1bc2-00e7-4a23-a403-504ea00906c1")]
 
 // Version information for an assembly consists of the following four values:
 //
